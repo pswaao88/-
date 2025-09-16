841bc87604714a3fc557d53b6b401bfe35dc77f9
pswaao88 <pswaao88@gmail.com>
2025-09-16 19:01:02 +0900

    feat: Student 클래스 작성

---
+++ test/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        Student psw = new Student("박상우");
+        psw.printName();
+    }
+}
---
+++ test/src/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    private String name;
+
+    public Student (String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void printName(){
+        System.out.println("안녕하세요, " + this.name + "입니다.");
+    }
+}
